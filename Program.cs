00dd2c5673d68b06f8fdf018ff8af0667c720423
Michael Nascimento <michael.nsc@outlook.com>
2022-06-15 20:47:53 -0300

    Upcasting Downcasting

--- Program.cs
+++ Program.cs
@@ -5,17 +5,23 @@ class Program
 {
   static void Main(string[] args)
   {
-    var boleto = new PagamentoBoleto();
+    PagamentoBoleto boleto = new PagamentoBoleto();
     boleto.DataVencimento = new DateTime(2022, 06, 14); // antiga
     boleto.Pagar(100.00M);
 
-    var boleto2 = new PagamentoBoleto();
+    PagamentoBoleto boleto2 = new PagamentoBoleto();
     boleto2.DataVencimento = new DateTime(2022, 06, 20); // futura
     boleto2.Pagar(100.00M);
 
-    var pix = new PagamentoPix();
+    PagamentoPix pix = new PagamentoPix();
     pix.DataVencimento = new DateTime(2022, 06, 10); // antiga
     pix.Pagar(49.99M);
 
+    Pagamento umPagamento = new PagamentoBoleto();
+    Pagamento outroPagamento = new PagamentoPix();
+
+    PagamentoBoleto pagamentoDowncasting = (PagamentoBoleto)new Pagamento();
+
+    IList<Pagamento> listaPagamentos = new List<Pagamento>();
   }
 }
